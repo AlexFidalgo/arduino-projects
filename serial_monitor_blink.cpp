503522e675db0e49689587d9b3f607ec62a515d5
Alex Fidalgo <67179630+AlexFidalgo@users.noreply.github.com>
2023-04-29 13:10:23 -0300

    Update serial_monitor_blink.cpp

--- serial_monitor_blink.cpp
+++ serial_monitor_blink.cpp
@@ -24,3 +24,17 @@ void loop() {
     }
   }
 }
+
+/*
+To send data through the serial monitor in the Arduino IDE, follow these steps:
+
+Open the Arduino IDE and connect your Arduino board to your computer using a USB cable.
+In the Arduino IDE, select the appropriate board and serial port from the "Tools" menu.
+Open a new sketch or load an existing one that includes serial communication code.
+In your code, initialize the serial communication using the Serial.begin() function. For example, you could use Serial.begin(9600) to set the baud rate to 9600 bits per second.
+In your code, use the Serial.print() or Serial.println() function to send data to the serial monitor. For example, you could use Serial.println("Hello, world!") to send the string "Hello, world!" to the serial monitor.
+Upload the code to your Arduino board using the "Upload" button in the Arduino IDE.
+Open the serial monitor by clicking the magnifying glass icon in the top right corner of the Arduino IDE, or by selecting "Tools" -> "Serial Monitor" from the menu.
+In the serial monitor, select the appropriate baud rate from the dropdown menu in the bottom right corner. This should match the baud rate you set in your code using the Serial.begin() function.
+Send data to the Arduino by typing into the serial monitor's input field at the top of the window and pressing "Enter" or "Return". The Arduino should receive the data and respond accordingly based on your code.
+*/
